a46e9b642104e9b7f36aaece7fcbb769c20bba8e
Majllo <miloszszelka01@gmail.com>
2023-12-13 19:29:32 +0100

    fixed nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -5,11 +5,13 @@ const allNavItems = document.querySelectorAll(".navbar__link")
 const showMenu = () => {
 	burgerBtn.classList.toggle("is-active")
 	menu.classList.toggle("show-menu")
+	document.body.classList.toggle("fixed-body")
 
 	allNavItems.forEach(item => {
 		item.addEventListener("click", () => {
 			menu.classList.remove("show-menu")
 			burgerBtn.classList.remove("is-active")
+			document.body.classList.remove("fixed-body")
 		})
 	})
 }
@@ -18,6 +20,7 @@ const deleteClassOnDesktop = function () {
 	if (992 < window.innerWidth) {
 		menu.classList.remove("show-menu")
 		burgerBtn.classList.remove("is-active")
+		document.body.classList.remove("fixed-body")
 	}
 }
 
